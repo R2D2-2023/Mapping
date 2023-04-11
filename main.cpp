97a2b727ba730f25398d18c2fbb503ededf89daf
Jameson Lin <94827857+jamesonLin@users.noreply.github.com>
2023-04-11 15:55:30 +0200

    updated the code, updates the image per change

--- main.cpp
+++ main.cpp
@@ -8,36 +8,23 @@
 #include <cstdlib>
 #include <string>
 
+
 struct MouseData {
     std::vector<std::vector<int>>* matrix;
     int cell_size;
+    cv::Mat* img;
 };
 
-void onMouse(int event, int x, int y, int flags, void* userdata) {
-    MouseData* data = (MouseData*)userdata;
-    int cell_size = data->cell_size;
-    std::vector<std::vector<int>>* matrix = data->matrix;
-    int j = x / cell_size;
-    int i = y / cell_size;
-    if (i >= 0 && i < matrix->size() && j >= 0 && j < matrix->at(0).size()) {
-        matrix->at(i).at(j) = 1;
-    }
-}
-
-std::vector<std::vector<int>> createGridImage(cv::Mat& img, std::vector<std::vector<int>> matrix){
-    int longest_dim = std::max(img.cols, img.rows); 
-    int cell_size = std::ceil((double)longest_dim / 120);
-    int size = std::ceil((double)longest_dim / cell_size);
-
-
+void updateGridImage(cv::Mat& img, int cell_size, std::vector<std::vector<int>>& matrix) {
     cv::Scalar grid_color = cv::Scalar(0, 0, 255);
+    cv::Scalar grid_color_green = cv::Scalar(0, 255, 0);
     cv::Mat grid_img = img.clone();
-    for (int i = 0; i < size; i++){
+    for (int i = 0; i < matrix.size(); i++){
         int y = i * cell_size;
-        for (int j = 0; j < size; j++){
+        for (int j = 0; j < matrix.at(0).size(); j++){
             int x = j * cell_size;
             if(matrix[i][j] == 1){
-                cv::rectangle(grid_img, cv::Point(x, y), cv::Point(x + cell_size, y + cell_size), grid_color, -1);
+                cv::rectangle(grid_img, cv::Point(x, y), cv::Point(x + cell_size, y + cell_size), grid_color_green, -1);
             }
             else{
                 cv::rectangle(grid_img, cv::Point(x, y), cv::Point(x + cell_size, y + cell_size), grid_color, 1);
@@ -45,17 +32,33 @@ std::vector<std::vector<int>> createGridImage(cv::Mat& img, std::vector<std::vec
         }
     }
 
+    cv::imshow("Grid Image", grid_img);
+}
+
+void onMouse(int event, int x, int y, int flags, void* userdata) {
+    if(event == cv::EVENT_MOUSEMOVE && (flags & cv::EVENT_FLAG_LBUTTON)){
+        MouseData* data = (MouseData*)userdata;
+        int cell_size = data->cell_size;
+        std::vector<std::vector<int>>* matrix = data->matrix;
+        int j = x / cell_size;
+        int i = y / cell_size;
+        if (i >= 0 && i < matrix->size() && j >= 0 && j < matrix->at(0).size()) {
+            matrix->at(i).at(j) = 1;
+            updateGridImage(*data->img, cell_size, *matrix);
+        }
+    }
+}
+
+std::vector<std::vector<int>> createGridImage(cv::Mat& img, int longest_dim, int cell_size, int size, std::vector<std::vector<int>> matrix){
+    cv::namedWindow("Grid Image", cv::WINDOW_NORMAL);
+
     MouseData data;
     data.matrix = &matrix;
     data.cell_size = cell_size;
-    cv::namedWindow("Grid Image", cv::WINDOW_NORMAL);
-    cv::setMouseCallback("Grid Image", onMouse, &data);
-    cv::imshow("Grid Image", grid_img);
+    data.img = &img;
 
-    if (grid_img.empty() || grid_img.cols <= 0 || grid_img.rows <= 0) {
-        std::cerr << "Error: Grid image is empty or has zero dimensions." << std::endl;
-        exit(-1);
-    }
+    cv::setMouseCallback("Grid Image", onMouse, &data);
+    updateGridImage(img, cell_size, matrix);
 
     cv::waitKey(0);
 
@@ -72,17 +75,8 @@ int main(){
     int cell_size = std::ceil((double)longest_dim / 120);
     int size = std::ceil((double)longest_dim / cell_size);
 
-    std::vector<std::vector<int>> matrix( size , std::vector<int> (size, 0));  ;
-    matrix = createGridImage(img, matrix);
-    for(auto lst : matrix){
-
-        for(auto e : lst){
-               std::cout << e << " ";
-        }
-        std::cout << std::endl;
-    }
-
-    matrix = createGridImage(img, matrix);
+    std::vector<std::vector<int>> matrix( size , std::vector<int> (size, 0));
+    matrix = createGridImage(img, longest_dim, cell_size, size, matrix);
 
     return 0;
 }
