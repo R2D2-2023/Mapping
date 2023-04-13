491f609e81523695d55ed89ae5d0e6da38178ce4
Jameson Lin <94827857+jamesonLin@users.noreply.github.com>
2023-04-13 13:14:55 +0200

    Added doxygen in code

--- main.cpp
+++ main.cpp
@@ -8,6 +8,11 @@
 #include <cstdlib>
 #include <string>
 
+/**
+ * @file main.cpp
+ * 
+ */
+
 
 struct MouseData {
     std::vector<std::vector<int>>* matrix;
@@ -15,7 +20,17 @@ struct MouseData {
     cv::Mat* img;
 };
 
-void updateGridImage(cv::Mat& img, int cell_size, std::vector<std::vector<int>>& matrix) {
+/**
+ * @brief for the grid making and updating
+ * 
+ * @param img 
+ * @param cell_size 
+ * @param matrix 
+ * 
+ * @details is used to update the grid based on the mouse
+ * 
+ */
+void UpdateGridImage(cv::Mat& img, int cell_size, std::vector<std::vector<int>>& matrix) {
     cv::Scalar grid_color = cv::Scalar(0, 0, 255);
     cv::Scalar grid_color_green = cv::Scalar(0, 255, 0);
     cv::Mat grid_img = img.clone();
@@ -35,8 +50,21 @@ void updateGridImage(cv::Mat& img, int cell_size, std::vector<std::vector<int>>&
     cv::imshow("Grid Image", grid_img);
 }
 
-void onMouse(int event, int x, int y, int flags, void* userdata) {
-    MouseData* data = (MouseData*)userdata;
+/**
+ * @brief For mouse movement and events
+ * 
+ * @param event 
+ * @param x 
+ * @param y 
+ * @param flags 
+ * @param user_data 
+ * 
+ * @details this is for the movement of the mouse and button press of the mouse 
+ * @details so it can see where on the grid it is
+ * 
+ */
+void OnMouse(int event, int x, int y, int flags, void* user_data) {
+    MouseData* data = (MouseData*)user_data;
     int cell_size = data->cell_size;
     std::vector<std::vector<int>>* matrix = data->matrix;
     int j = x / cell_size;
@@ -44,18 +72,28 @@ void onMouse(int event, int x, int y, int flags, void* userdata) {
     if(event == cv::EVENT_MOUSEMOVE && (flags & cv::EVENT_FLAG_LBUTTON)){
         if (i >= 0 && i < matrix->size() && j >= 0 && j < matrix->at(0).size()) {
             matrix->at(i).at(j) = 1;
-            updateGridImage(*data->img, cell_size, *matrix);
+            UpdateGridImage(*data->img, cell_size, *matrix);
         }
     } 
     else if(event == cv::EVENT_MOUSEMOVE && (flags & cv::EVENT_FLAG_RBUTTON)){
          if (i >= 0 && i < matrix->size() && j >= 0 && j < matrix->at(0).size()) {
             matrix->at(i).at(j) = 0;
-            updateGridImage(*data->img, cell_size, *matrix);
+            UpdateGridImage(*data->img, cell_size, *matrix);
         }
     }
 }
 
-std::vector<std::vector<int>> createGridImage(cv::Mat& img, int longest_dim, int cell_size, int size, std::vector<std::vector<int>> matrix){
+/**
+ * @brief Create a Grid Image object
+ * 
+ * @param img 
+ * @param longest_dim 
+ * @param cell_size 
+ * @param size 
+ * @param matrix 
+ * @return std::vector<std::vector<int>> 
+ */
+std::vector<std::vector<int>> CreateGridImage(cv::Mat& img, int longest_dim, int cell_size, int size, std::vector<std::vector<int>> matrix){
     cv::namedWindow("Grid Image", cv::WINDOW_NORMAL);
 
     MouseData data;
@@ -63,13 +101,14 @@ std::vector<std::vector<int>> createGridImage(cv::Mat& img, int longest_dim, int
     data.cell_size = cell_size;
     data.img = &img;
 
-    cv::setMouseCallback("Grid Image", onMouse, &data);
-    updateGridImage(img, cell_size, matrix);
+    cv::setMouseCallback("Grid Image", OnMouse, &data);
+    UpdateGridImage(img, cell_size, matrix);
 
     cv::waitKey(0);
 
     return matrix;
 }
+
 
 int main(){
     cv::Mat img = cv::imread("map.png", cv::IMREAD_COLOR);
@@ -82,7 +121,7 @@ int main(){
     int size = std::ceil((double)longest_dim / cell_size);
 
     std::vector<std::vector<int>> matrix( size , std::vector<int> (size, 0));
-    matrix = createGridImage(img, longest_dim, cell_size, size, matrix);
+    matrix = CreateGridImage(img, longest_dim, cell_size, size, matrix);
 
     return 0;
 }
