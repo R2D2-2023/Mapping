ad406998845d463f48fcb12a0dc13c18b54c877b
BasvandenBergh <basbergh@outlook.com>
2023-06-26 14:08:40 +0200

    een nieuw bestand met een leeg grid om later de text file uit te lezen

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ class MouseData:
         self.checkpoint_counter = -5
         self.route = []
         self.meetPunten = []
+        self.hoeken = []
         self.robotX = 10
         self.robotY = 10
         self.mode = 0
@@ -33,6 +34,7 @@ def update_grid_image(matrix, data):
     grid_color = (0, 0, 255)
     grid_color_green = (0, 255, 0)
     grid_color_checkpoint = (255, 0, 255)
+    grid_color_corner = (255, 0, 0)
     grid_img = data.MData.origImg.copy()
     cell_size = data.MData.cell_size
 
@@ -46,57 +48,30 @@ def update_grid_image(matrix, data):
                 rect_color = grid_color
             elif value == 9999:
                 rect_color = (100, 100, 100)
+            elif value == 6000:
+                rect_color = grid_color_corner
             else:
                 if value < 0:
                     rect_color = grid_color_checkpoint
                 else:
                     rect_color = grid_color_green
             cv2.rectangle(grid_img, (x, y), (x + cell_size, y + cell_size), rect_color, 1 if value == 0 else -1)
-    cv2.imshow("Grid Image", grid_img)
-
-
-
-def check_surroundings(data):
-    next_found = False
-    for y in range(data.MData.robotY, data.MData.robotY + 1):
-        for x in range(data.MData.robotX, data.MData.robotX + 1):
-            if x < 0 or x >= 65 or y < 0 or y >= 232:
-                continue
-            if len(data.MData.route) <= data.MData.counter:
-                return 0
-            elif x == data.MData.route[data.MData.counter][0] and y == data.MData.route[data.MData.counter][1]:
-                value = data.MData.matrix[x][y]
-                data.MData.counter += 1
-                print("COORDINATE FOUND")
-
-                if value == -1:
-                    # Coördinaat is een obstakel
-                    return -1
-                elif value == -2:
-                    # Coördinaat is een checkpoint
-                    return -2
-                else:
-                    # Coördinaat is een geldige volgende stap
-                    return 1
-            else:
-                next_found = True
-
-    if not next_found:
-        # Er zijn geen volgende coördinaten in de route
-        return 0
     
-def edit_image():
+    cv2.imshow("Grid Image", grid_img)
+    
+def edit_image(filename):
     screenWidth = GetSystemMetrics(0)
     screenHeight = GetSystemMetrics(1)
 
-    img = cv2.imread("Toerk_map.png", cv2.IMREAD_COLOR)
+    img = cv2.imread(filename, cv2.IMREAD_COLOR)
     if img is None:
         print("Could not read the image")
 
     imageAspectRatio = img.shape[1] / img.shape[0]
+    
     maxWidth = screenWidth
     maxHeight = screenHeight
-
+ 
     scaledWidth = maxWidth
     scaledHeight = int(scaledWidth / imageAspectRatio)
 
@@ -107,112 +82,13 @@ def edit_image():
     posX = (screenWidth - scaledWidth) // 2
     posY = (screenHeight - scaledHeight) // 2
 
+    cv2.namedWindow("Grid Image")
     cv2.moveWindow("Grid Image", posX, posY)
     img = cv2.resize(img, (scaledWidth, scaledHeight))
 
     return img
 
-def OnMouse(event, x, y, flags, user_data):
-    data = user_data
-    cell_size = data.MData.cell_size
-    matrix = data.MData.matrix
-    matrix_width = len(matrix[0])
-
-    j = (x - data.gData.beginX) // cell_size
-    i = (y - data.gData.beginY) // cell_size
-
-    if event == cv2.EVENT_MOUSEMOVE:
-        if i >= 0 and i < len(matrix) and j >= 0 and j < matrix_width:
-            current_cell = matrix[i][j]
-            if data.MData.mode == 0:
-                if flags & cv2.EVENT_FLAG_LBUTTON:
-                    if current_cell == 0 and current_cell != 1:
-                        data.MData.counter += 1
-                        current_cell = data.MData.counter
-                        data.MData.route.append((i, j))
-                        print(f"{j}/{i}: {data.MData.counter}")
-                        print("route: ", end="")
-                        route_ss = ""
-                        for route_coord in data.MData.route:
-                            route_ss += f"{route_coord[0]}/{route_coord[1]},"
-                        print(route_ss)
-                        update_grid_image(matrix, data)
-                elif flags & cv2.EVENT_FLAG_RBUTTON:
-                    if current_cell != 0:
-                        is_in_route_or_meetpunten = False
-                        route_it = next((index for index, coord in enumerate(data.MData.route) if coord == (i, j)), None)
-                        meetpunten_it = next((index for index, coord in enumerate(data.MData.meetPunten) if coord == (i, j)), None)
-                        if route_it is not None or meetpunten_it is not None:
-                            is_in_route_or_meetpunten = True
-                            if route_it is not None:
-                                data.MData.route.pop(route_it)
-                            if meetpunten_it is not None:
-                                data.MData.meetPunten.pop(meetpunten_it)
-                        if not is_in_route_or_meetpunten:
-                            current_cell = 0
-                            update_grid_image(matrix, data)
-                check_surroundings(data)
-            else:
-                if flags & cv2.EVENT_FLAG_LBUTTON:
-                    current_cell = 9999
-                    data.MData.robotX = i
-                    data.MData.robotY = j
-                    update_grid_image(matrix, data)
-                elif flags & cv2.EVENT_FLAG_RBUTTON:
-                    if current_cell == 0:
-                        data.MData.checkpoint_counter -= 1
-                        current_cell = data.MData.checkpoint_counter
-                        data.MData.meetPunten.append((i, j))
-                        print("meetPunten: ", end="")
-                        meetpunten_ss = ""
-                        for meetpunten_coord in data.MData.meetPunten:
-                            meetpunten_ss += f"{meetpunten_coord[0]}/{meetpunten_coord[1]},"
-                        print(meetpunten_ss)
-                        update_grid_image(matrix, data)
-                check_surroundings(data)
-    elif event == cv2.EVENT_MOUSEWHEEL:
-        if i >= 0 and i < len(matrix) and j >= 0 and j < matrix_width:
-            print(data.MData.mode)
-            data.MData.mode = 1 if data.MData.mode == 0 else 0
-            print(f"mode: {data.MData.mode}")
-
-
-def create_grid_image(img, origImg, begin, end):
-
-    longest_dim = max(img.shape[1], img.shape[0])
-    cell_size = int(np.ceil(longest_dim / 300))
-
-    uData = StoreData()
-    uData.gData.beginX = begin[0]
-    uData.gData.beginY = begin[1]
-    uData.gData.endX = end[0]
-    uData.gData.endY = end[1]
-
-    roi = (begin[0], begin[1], end[0] - begin[0], end[1] - begin[1])
-    roiImage = img[roi[1]:roi[1]+roi[3], roi[0]:roi[0]+roi[2]]
-
-    width = int(np.ceil(roiImage.shape[1] / cell_size))
-    height = int(np.ceil(roiImage.shape[0] / cell_size))
-
-    matrix = [[0] * width for _ in range(height)]
-
-    uData.MData.matrix = matrix
-    uData.MData.cell_size = cell_size
-    uData.MData.img = img
-    uData.MData.origImg = origImg
-
-    cv2.setMouseCallback("Grid Image", OnMouse, uData)
-    update_grid_image(matrix, uData)
-
-    cv2.waitKey(0)
-
-    return matrix
-
-
-def main():
-    cv2.namedWindow("Grid Image")  # Create the named window before calling edit_image()
-    img = edit_image()
-
+def get_roi(img):
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     # Define the lower and upper bounds of the black color range
@@ -250,8 +126,176 @@ def main():
 
     startPoint = (leftmostPoint[0], highestPoint[1])  # Starting coordinate of the grid
     endPoint = (rightmostPoint[0], lowestPoint[1])  # Ending coordinate of the grid
+    
+    return maskImg, startPoint, endPoint
 
-    matrix = create_grid_image(maskImg, img, startPoint, endPoint)
+def OnMouse(event, x, y, flags, user_data):
+    data = user_data
+    cell_size = data.MData.cell_size
+    matrix = data.MData.matrix
+    matrix_width = len(matrix[0])
+
+    j = (x - data.gData.beginX) // cell_size
+    i = (y - data.gData.beginY) // cell_size
+
+    if event == cv2.EVENT_MOUSEMOVE:
+        if i >= 0 and i < len(matrix) and j >= 0 and j < matrix_width:
+            current_cell = matrix[i][j]  # Get the current value from the matrix
+            if data.MData.mode == 0:
+                if flags & cv2.EVENT_FLAG_LBUTTON:
+                    if current_cell == 0 and current_cell != 1:
+                        data.MData.counter += 1
+                        current_cell = data.MData.counter
+                        data.MData.route.append((i, j))
+                        print(f"{j}/{i}: {data.MData.counter}")
+                        print("route: ", end="")
+                        route_ss = ""
+                        for route_coord in data.MData.route:
+                            route_ss += f"{route_coord[0]}/{route_coord[1]},"
+                        print(route_ss)
+                        update_grid_image(matrix, data)
+                elif flags & cv2.EVENT_FLAG_RBUTTON:
+                    if current_cell != 0:
+                        is_in_route_or_meetpunten = False
+                        route_it = next((index for index, coord in enumerate(data.MData.route) if coord == (i, j)), None)
+                        meetpunten_it = next((index for index, coord in enumerate(data.MData.meetPunten) if coord == (i, j)), None)
+                        if route_it is not None or meetpunten_it is not None:
+                            is_in_route_or_meetpunten = True
+                            if route_it is not None:
+                                data.MData.route.pop(route_it)
+                            if meetpunten_it is not None:
+                                data.MData.meetPunten.pop(meetpunten_it)
+                        if not is_in_route_or_meetpunten:
+                            current_cell = 0
+                            update_grid_image(matrix, data)
+                check_surroundings(data)
+            elif data.MData.mode == 1:
+                if flags & cv2.EVENT_FLAG_LBUTTON:
+                    if current_cell == 0:
+                        current_cell = 9999
+                        data.MData.robotX = i
+                        data.MData.robotY = j
+                        update_grid_image(matrix, data)
+                elif flags & cv2.EVENT_FLAG_RBUTTON:
+                    if current_cell == 0:
+                        data.MData.checkpoint_counter -= 1
+                        current_cell = data.MData.checkpoint_counter
+                        data.MData.meetPunten.append((i, j))
+                        print("meetPunten: ", end="")
+                        meetpunten_ss = ""
+                        for meetpunten_coord in data.MData.meetPunten:
+                            meetpunten_ss += f"{meetpunten_coord[0]}/{meetpunten_coord[1]},"
+                        print(meetpunten_ss)
+                        update_grid_image(matrix, data)
+                check_surroundings(data)
+            elif data.MData.mode == 2:  # Add mode 2 logic here
+                if flags & cv2.EVENT_FLAG_LBUTTON:
+                    if current_cell == 0:
+                        current_cell = 6000
+                        data.MData.hoeken.append((i, j))
+                        print("hoeken: ", end="")
+                        hoeken_ss = ""
+                        for hoeken_coord in data.MData.hoeken:
+                            hoeken_ss += f"{hoeken_coord[0]}/{hoeken_coord[1]},"
+                        print(hoeken_ss)
+                        update_grid_image(matrix, data)
+                elif flags & cv2.EVENT_FLAG_RBUTTON:
+                        update_grid_image(matrix, data)
+                check_surroundings(data)
+                
+            matrix[i][j] = current_cell  # Update the value in the matrix
+
+    elif event == cv2.EVENT_MOUSEWHEEL:
+        if i >= 0 and i < len(matrix) and j >= 0 and j < matrix_width:
+            data.MData.mode = (data.MData.mode + 1) % 3  # Cycle through modes 0, 1, and 2
+            print(f"mode: {data.MData.mode}")
+
+def setData(img, begin, end, origImg):
+    longest_dim = max(img.shape[1], img.shape[0])
+    cell_size = int(np.ceil(longest_dim / 300))
+
+    uData = StoreData()
+    uData.gData.beginX = begin[0]
+    uData.gData.beginY = begin[1]
+    uData.gData.endX = end[0]
+    uData.gData.endY = end[1]
+
+    roi = (begin[0], begin[1], end[0] - begin[0], end[1] - begin[1])
+    roiImage = img[roi[1]:roi[1]+roi[3], roi[0]:roi[0]+roi[2]]
+
+    width = int(np.ceil(roiImage.shape[1] / cell_size))
+    height = int(np.ceil(roiImage.shape[0] / cell_size))
+
+    matrix = [[0] * width for _ in range(height)]
+
+    uData.MData.matrix = matrix
+    uData.MData.cell_size = cell_size
+    uData.MData.img = img
+    uData.MData.origImg = origImg
+    
+    return matrix, uData
+
+def write_to_txt(uData, filename):
+    # Convert tuples to strings
+    route_str = [f"{coord[0]}/{coord[1]}" for coord in uData.MData.route]
+    meetPunten_str = [f"{coord[0]}/{coord[1]}" for coord in uData.MData.meetPunten]
+    hoeken_str = [f"{coord[0]}/{coord[1]}" for coord in uData.MData.hoeken]
+
+    # Combine the arrays into a single string
+    data = ",".join(route_str) + "\n" + ",".join(meetPunten_str) + "\n" + ",".join(hoeken_str)
+    
+    with open(filename, "w") as file:
+        file.write(data)
+
+
+def create_grid_image(filename):
+    
+    screenWidth = GetSystemMetrics(0)
+    screenHeight = GetSystemMetrics(1)
+
+    origImg = cv2.imread(filename, cv2.IMREAD_COLOR)
+    if origImg is None:
+        print("Could not read the image")
+
+    imageAspectRatio = origImg.shape[1] / origImg.shape[0]
+    
+    maxWidth = screenWidth
+    maxHeight = screenHeight
+ 
+    scaledWidth = maxWidth
+    scaledHeight = int(scaledWidth / imageAspectRatio)
+
+    if scaledHeight > maxHeight:
+        scaledHeight = maxHeight
+        scaledWidth = int(scaledHeight * imageAspectRatio)
+
+    posX = (screenWidth - scaledWidth) // 2
+    posY = (screenHeight - scaledHeight) // 2
+
+    cv2.namedWindow("Grid Image")
+    cv2.moveWindow("Grid Image", posX, posY)
+    origImg = cv2.resize(origImg, (scaledWidth, scaledHeight))
+    
+    
+    img, begin, end = get_roi(origImg)
+    
+
+    matrix, uData = setData(img, begin, end, origImg)
+    
+
+    cv2.setMouseCallback("Grid Image", OnMouse, uData)
+    update_grid_image(matrix, uData)
+
+    cv2.waitKey(0)
+    
+    write_to_txt(uData, "arrays.txt")
+
+    return matrix
+
+
+def main():
+    
+    matrix = create_grid_image("Toerk_map.png")
 
 
 if __name__ == '__main__':
